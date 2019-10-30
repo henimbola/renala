9a8cc77dc2b2fe148bf44a1db4db85e969f8a5a8
nary <narisoaandrians@gmail.com>
2019-10-30 14:37:54 +0300

    [UPDATE] l10n_mg_partner, renala

--- renala/__manifest__.py
+++ renala/__manifest__.py
@@ -3,7 +3,7 @@
     'name': "renala",
 
     'summary': """
-        Modèle de Facture""",
+        Personnalistion RENALA""",
 
     'description': """
         Modèle de facture customisée
@@ -13,23 +13,12 @@
     'author': "Ingenosya Madagascar",
     'website': "http://www.ingenosya.mg",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/10.0/odoo/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Uncategorized',
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
     'depends': ['base','l10n_mg_partner','account'],
 
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+        'views/res_partner.xml',
+        'report/account_invoice.xml',
     ],
 }
